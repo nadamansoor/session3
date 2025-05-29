cb2c84e7eb3c80e9f883aacee6479cfe4464b2e1
nada mansour <nadamansour1566@gmail.com>
2025-05-29 11:18:52 +0300

    first commit

---
+++ bin/session3.dart
@@ -0,0 +1,5 @@
+import 'package:session3/session3.dart' as session3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${session3.calculate()}!');
+}
---
+++ test/session3_test.dart
@@ -0,0 +1,8 @@
+import 'package:session3/session3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
