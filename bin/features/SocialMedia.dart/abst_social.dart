6d6e26f81cd5e2385a066994b72ccd6a1428c830
nada mansour <nadamansour1566@gmail.com>
2025-05-29 13:36:18 +0300

    abst

--- bin/features/SocialMedia.dart/abst_social.dart
+++ bin/features/SocialMedia.dart/abst_social.dart
@@ -3,13 +3,16 @@ List<String> posts=[
   'this is share'
 ];
 
-// abstract class
+// abstract class =,base
 abstract class social{
   // signature
   Future<String> getPosts();
+  Future<String> deletePost(int id);
+  Future<String> addPost(String post);
+
 
 }
-
+//implementation
 class socialmediaImplementation extends social{
   
   @override
@@ -19,14 +22,34 @@ class socialmediaImplementation extends social{
     }
     return Future.value('success');
   }
+  
+  @override
+  Future<String> deletePost(int id) {
+     posts.removeAt(id);
+    return Future.value('success');
+  }
+  
+  @override
+  Future<String> addPost(String post) {
+    posts.add(post);
+    return Future.value('success');
+  }
 
 }
-
+// screen
 class UiRrnder{
   final social sm;
   UiRrnder(this.sm);
 
   void showPosts(){
     sm.getPosts();
+  }
+
+  void deleteButton(int id){
+    sm.deletePost(id);
+  }
+
+  void addPostButton(String post){
+    sm.addPost(post);
   }
 }
--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,20 @@
 import 'features/transactions/enc_trans.dart';
 import 'features/SocialMedia.dart/abst_social.dart';
+
 void main(List<String> arguments) {
 
-  // Bank bk =Bank();
-  // bk.getBalance;
-  // bk.deposit=5000;
-  // bk.getBalance;
-  // bk.withdrow=300;
-  // bk.getBalance;
+  Bank bk =Bank();
+  bk.getBalance;
+  bk.deposit=5000;
+  bk.getBalance;
+  bk.withdrow=300;
+  bk.getBalance;
+
 
   UiRrnder ui = UiRrnder(socialmediaImplementation());
   ui.showPosts();
-
+  ui.deleteButton(0);
+  ui.showPosts();
+  ui.addPostButton('she is a woman, ');
+  ui.showPosts();
   }
